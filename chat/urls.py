8f36f13181c2bee76825e493b42fb6cc8eb2fe63
dandeboer <ddeboer926@gmail.com>
2020-03-25 18:14:02 -0400

    committing so rebecca can see it

---
+++ chat/urls.py
@@ -0,0 +1,12 @@
+# chat/urls.py
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.all_rooms, name="all_rooms"),
+    url(r'rooms/(?P<slug>[-\w]+)/$', views.room_detail, name="room_detail"),
+    url(r'^$', views.all_rooms, name="all_rooms"),
+    url(r'token$', views.token, name="token"),
+    url(r'rooms/(?P<slug>[-\w]+)/$', views.room_detail, name="room_detail"),
+]
---
+++ chatroom_app/urls.py
@@ -0,0 +1,9 @@
+# chatroom_app/urls.py
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+   url(r'^admin/', admin.site.urls),
+   url(r'^', include('chat.urls')),
+]
